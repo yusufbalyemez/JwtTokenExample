78054452dcdaa945dec1ead4f326acab11f4ed36
Yusuf Balyemez <yusuf.balyemez93@gmail.com>
2024-02-07 10:42:13 +0300

    Login işlemi yapıldı.

--- app/index.js
+++ app/index.js
@@ -14,6 +14,30 @@ dotenv.config()
 //Bir server uygulaması oluşturur.
 const app = express()
 
+//Expressin json verileri çağrıldı. Bir middleware döndürüyor. Content-Type başlığının eşleştiği isteklere bakıyor.
+app.use(express.json())
+
+//Bu içerikte veritabanı kullanılmadı. Onun yerine bu yapı kullanıldı. Normalde sizler veritabanı kullanabilirsiniz. Aynı mantık
+//Örnek veritabanı
+const user = {
+    username: "admin",
+    email: "admin@gmail.com",
+    password: "1234"
+}
+
+
+//Giriş servisi
+//Buraya istek atıldığında işlenecek veriler.
+app.post("/login", async(req,res)=>{
+
+    //gelen isteğin body'sinden verileri çekip değişkenlere aktarır.
+    const {email,password} = req.body; 
+
+    if(email !== user.email || password !==user.password){
+        return res.status(401).json({message: "Bilgiler geçersiz."})
+    }
+})
+
 //Bu server'ı 5000. portta dinlemesini ister.
 app.listen(5000,()=>{
     console.log("Server 5000. portta hazır...")
